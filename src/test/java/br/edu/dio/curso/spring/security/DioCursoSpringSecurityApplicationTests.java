288b212ad0dee99cf1fd10415d68432c42f6afb2
jpmjunior <junior.linhares@yahoo.com.br>
2024-10-11 16:46:26 -0300

    primeiro commit

---
+++ src/test/java/br/edu/dio/curso/spring/security/DioCursoSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.dio.curso.spring.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioCursoSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
