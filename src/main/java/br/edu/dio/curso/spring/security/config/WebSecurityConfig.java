12ae30408965ec66dfbb2fe99d69a4cea0ff8ffb
jpmjunior <junior.linhares@yahoo.com.br>
2024-10-11 21:35:56 -0300

    Serviço de autenticação via banco dados e inicialização da tabela de usuários

--- src/main/java/br/edu/dio/curso/spring/security/config/WebSecurityConfig.java
+++ src/main/java/br/edu/dio/curso/spring/security/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package br.edu.dio.curso.spring.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,12 +8,20 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private SecurityDatabaseService securityService;
+
+    @Autowired
+    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(securityService).passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
 
     // Configura perfis de acesso para as rotas da API e forma de autenticação (ex: formulário ou basic)
     @Override
@@ -22,20 +31,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST,"/login").permitAll()
                 .antMatchers("/managers").hasAnyRole("MANAGERS")
                 .antMatchers("/users").hasAnyRole("USERS","MANAGERS")
-                .anyRequest().authenticated().and().formLogin();
+                .anyRequest().authenticated().and().httpBasic();
     }
 
-    // Configura usuários para autenticação em memória
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user")
-                .password("{noop}user123")
-                .roles("USERS")
-                .and()
-                .withUser("admin")
-                .password("{noop}master123")
-                .roles("MANAGERS");
-    }
+//    // Configura usuários para autenticação em memória
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("user")
+//                .password("{noop}user123")
+//                .roles("USERS")
+//                .and()
+//                .withUser("admin")
+//                .password("{noop}master123")
+//                .roles("MANAGERS");
+//    }
 
 }
