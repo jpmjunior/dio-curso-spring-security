736c6d18d7917837e1754c35daf9af8c74aec7ff
jpmjunior <junior.linhares@yahoo.com.br>
2024-10-12 15:37:36 -0300

    Inclusão UserDTO e ModelMapper

--- src/main/java/br/edu/dio/curso/spring/security/controller/UserController.java
+++ src/main/java/br/edu/dio/curso/spring/security/controller/UserController.java
@@ -1,7 +1,9 @@
 package br.edu.dio.curso.spring.security.controller;
 
+import br.edu.dio.curso.spring.security.dto.UserDTO;
 import br.edu.dio.curso.spring.security.model.User;
 import br.edu.dio.curso.spring.security.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,9 +20,13 @@ public class UserController {
     @Autowired
     private UserService service;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     @PostMapping
-    public ResponseEntity postUser(@RequestBody User user){
-        service.createUser(user);
+    public ResponseEntity postUser(@RequestBody UserDTO userDTO){
+        User userEntity = modelMapper.map(userDTO, User.class);
+        service.createUser(userEntity);
         return ResponseEntity.created(URI.create("/users")).build();
     }
 
